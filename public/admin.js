89a368d78199a4757a1fdf62c24ba678d0eb61e8
Jennifer Herdzina <jen@Jennifers-MacBook-Pro.local>
2022-02-28 19:19:41 -0600

    push class assignment changes for admin.js file

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,42 @@
+async function main() {    
+    let response = await fetch('http://localhost:3001/listBooks');
+    let books = await response.json();
+    books.forEach(renderBook);
+}
 
-// Your Code Here
+function renderBook(book) {
+    let item = document.createElement('li');
+    item.textContent = book.title;
+
+    let quantity = document.createElement('input');
+    quantity.value = book.quantity;
+    quantity.setAttribute('id', 'qty' + book.id)
+
+    let button = document.createElement('button');
+    button.textContent = 'Save'
+    button.setAttribute('id', book.id)
+    button.addEventListener('click', updateBook);
+
+    item.append(quantity, button);
+    list.append(item);
+}
+
+async function updateBook(e) {
+    let response = await fetch('http://localhost:3001/updateBook', {
+        method: "PATCH",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+                "id": parseInt(e.target.id),
+                "quantity": parseInt(document.getElementById('qty' + e.target.id).value)
+        }),
+    });
+}
+
+let root = document.querySelector('#root');
+let list = document.createElement('ul');
+root.append(list);
+
+main();
+
